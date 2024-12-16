520843362f9ebd6362e309e5189560cf133ff766
1y4nu <akanoim@gmail.com>
2024-12-16 19:05:59 -0400

    Changed Pane to VBox for the Playing Field

--- src/builders/TicTacToeBuilder.java
+++ src/builders/TicTacToeBuilder.java
@@ -1,8 +1,10 @@
 package builders;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 
 public class TicTacToeBuilder {
     private Runnable handler;
@@ -13,15 +15,17 @@ public class TicTacToeBuilder {
 
     public Region build() {
         GridPane pane = new GridPane();
-
         for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col ++) {
+            for (int col = 0; col < 3; col++) {
                 Button button = new Button();
-                button.setPrefSize(100,100);
-                button.setOnAction(evt -> handler.run());
+                button.setPrefSize(70, 70); // Set preferred size for each button
+                button.setOnAction(e -> handler.run()); // Set the click handler
                 pane.add(button, col, row);
             }
         }
-        return pane;
+        VBox vbox = new VBox();
+        vbox.getChildren().add(pane);
+        vbox.setAlignment(Pos.CENTER_RIGHT);
+        return vbox;
     }
 }
