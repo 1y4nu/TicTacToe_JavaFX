1a4803bd5ccbb36cab0a18f6d68b8c0945b75568
1y4nu <akanoim@gmail.com>
2024-12-16 16:07:31 -0400

    This commit will add the 2D Array of buttons in the Builder Class

--- src/builders/TicTacToeBuilder.java
+++ src/builders/TicTacToeBuilder.java
@@ -1,4 +1,27 @@
 package builders;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+
 public class TicTacToeBuilder {
+    private Runnable handler;
+
+    public TicTacToeBuilder(Runnable handler) {
+        this.handler = handler;
+    }
+
+    public Region build() {
+        GridPane pane = new GridPane();
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col ++) {
+                Button button = new Button();
+                button.setPrefSize(100,100);
+                button.setOnAction(evt -> handler.run());
+                pane.add(button, col, row);
+            }
+        }
+        return pane;
+    }
 }
